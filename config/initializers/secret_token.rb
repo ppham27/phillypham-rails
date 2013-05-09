0fd0b79c5831a7839616d78093deb5a795bd67d3
Philip Pham <phil@Philips-MacBook-Pro.local>
2013-05-08 22:27:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phillypham::Application.config.secret_token = 'a77e9fe754d90096852962118480beb4c6b81c094cdfb0a04676c448f3563a7c910cf4dcfecdb91f21c47b8f5be27d236c1dc8e0dfc3f47deda91ea7873ace18'
