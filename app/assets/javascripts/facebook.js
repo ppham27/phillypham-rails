c5bbfdd62b3c12e54786122e6c099bbce1f9a169
Philip Pham <ppham@sessionm.com>
2013-06-21 10:23:30 -0400

    changed facebook to english

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -2,6 +2,6 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=475283969228455";
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=475283969228455";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
